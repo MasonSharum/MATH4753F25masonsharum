d2d3b2490748075a4898b41d21b7c15a5f36d828
MasonSharum <masonsharum@gmail.com>
2025-10-01 13:16:22 -0500

    birthday function

---
+++ R/birthday.R
@@ -0,0 +1,12 @@
+#' Birthday function
+#'
+#' @param x
+#'
+#' @returns The chance there is a shared birthday among x people
+#' @export
+#'
+#' @examples
+#' birthday(20:24)
+birthday <- function(x){
+  1 - exp(lchoose(365,x) + lfactorial(x) - x*log(365))
+}
